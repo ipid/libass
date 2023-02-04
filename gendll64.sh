7034be01d30cea61de53a475987241f1daf1e110
ipid <ipid@users.noreply.github.com>
2023-02-04 16:55:21 +0800

    [WIP] 更新 gendll64.sh 脚本，使其依赖 dll

--- gendll64.sh
+++ gendll64.sh
@@ -19,14 +19,8 @@ if [ "$1" == "--clean" ]; then
 fi
 
 make -j8
+g++ -shared -o accupos64.dll libass/.libs/*.o libass/x86/.libs/*.o accupos/.libs/*.o -Wl,--output-def=accupos64.def  -Wl,-static -static-libgcc -static-libstdc++ -Wl,--version-script=version-script.txt -Wl,--retain-symbols-file=retain-symbols-file.txt -lm -liconv -lfreetype -lbrotlidec -lharfbuzz -lfribidi -lfontconfig -lexpat -lfreetype -lpng -lbz2 -lz -lgdi32
 
-(
-    g++ -shared -o accupos64.dll libass/.libs/*.o libass/x86/.libs/*.o accupos/.libs/*.o -Wl,--output-def=accupos64.def  -Wl,-static -static-libgcc -static-libstdc++ -Wl,--version-script=version-script.txt -Wl,--retain-symbols-file=retain-symbols-file.txt -lm -liconv -lfreetype -lbrotlidec -lharfbuzz -lfribidi -lfontconfig -lexpat -lfreetype -lpng -lbz2 -lz -lgdi32
-
-) & (
-    mkdir -p test/.libs
-
-    gcc -std=gnu99 -Wall -O2 -fPIC -static-libgcc -static-libstdc++ -c -o test/.libs/test.o test/test.c
-
-    g++ -static -o accupos64.exe test/.libs/test.o libass/.libs/*.o libass/x86/.libs/*.o accupos/.libs/*.o -Wl,-static -static-libgcc -static-libstdc++ -lm -liconv -lfreetype -lbrotlidec -lharfbuzz -lfribidi -lfontconfig -lexpat -lfreetype -lpng -lbz2 -lz -lgdi32
-)
+mkdir -p test/.libs
+gcc -std=gnu99 -Wall -O2 -fPIC -static-libgcc -static-libstdc++ -c -o test/.libs/test.o test/test.c
+gcc -static -static-libgcc -static-libstdc++ -o accupos64.exe test/.libs/test.o accupos64.dll
